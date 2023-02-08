435f9b0313de1ad14830091b723688a1ec6c4818
Sabbir <64514438+shmVirus@users.noreply.github.com>
2023-02-08 20:57:08 +0600

    solve codeforces 630A

---
+++ CodeForces/630A--Again_Twenty_Five.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main(void) {
+    long long int n;
+    std:: cin >> n;
+    // since the (only) last digit is 5, so it's always 25
+    std:: cout << "25\n";
+    return 0;
+}
