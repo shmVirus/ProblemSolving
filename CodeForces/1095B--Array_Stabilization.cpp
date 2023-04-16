ce156bc414c94232a8849d9a8d9acfa40359cb19
Sabbir <64514438+shmVirus@users.noreply.github.com>
2023-04-16 20:21:47 +0600

    solve codeforces 1095B

---
+++ CodeForces/1095B--Array_Stabilization.cpp
@@ -0,0 +1,25 @@
+// author: @shmVirus; Sabbir < sabbir@disroot.org >
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define nl cout << "\n";
+#define no cout << "NO\n";
+#define yes cout << "YES\n";
+
+void solution();
+
+int main (void) {
+    FastIO;
+    solution();
+    return 0;
+}
+void solution () {
+    int n; cin >> n; int a[n];
+    for (int i=0; i<n; i++) {
+        cin >> a[i];
+    }
+    sort(a, a+n);
+    cout << min(a[n-2]-a[0], a[n-1]-a[1]) << '\n';
+}
