27a6e021bab66e4d3ca0c4d8cf3f112f4a9f00db
Sabbir <64514438+shmVirus@users.noreply.github.com>
2023-04-18 19:54:31 +0600

    solve codeforces 1814A

---
+++ CodeForces/1814A--Coins.cpp
@@ -0,0 +1,25 @@
+// author: @shmVirus; Sabbir < sabbir@disroot.org >
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define nl cout << "\n";
+#define no cout << "NO\n";
+#define yes cout << "YES\n";
+
+void solution();
+
+int main (void) {
+    FastIO;
+    solution();
+    return 0;
+}
+void solution () {
+    long long t, k,n; cin >> t;
+    while (t--) {
+        cin >> n >> k;
+        if (n%2==0 || k%2==1) yes
+        else no
+    }
+}
