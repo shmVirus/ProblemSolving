b8616b921fabe925732b0480a54fad545d8a54ec
Sabbir <sabbir@disroot.org>
2023-07-10 21:23:50 +0600

    solve codeforces 1367A

---
+++ CodeForces/1367A--Short_Substrings.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastIO  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define nl      "\n"
+#define MULTITEST 1
+
+void solution () {
+    string b; cin >> b;
+    string a = b.substr(0, 2);
+
+    for (int i=3; i<b.size(); i+=2) {
+		a += b[i];
+	}
+    cout << a << nl;
+}
+
+int main (void) {
+    FastIO;
+    int tc=1; if (MULTITEST) cin>>tc;
+    for(int cs=1;cs<=tc;cs++) solution();
+    return 0;
+}
