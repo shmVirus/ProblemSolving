e6fe46238f5aaf439b46ca338249dd4d292a258e
Sabbir <sabbir@disroot.org>
2023-07-15 20:20:11 +0600

    solve codeforces 1283A

---
+++ CodeForces/1283A--Minutes_Before_the_New_Year.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastIO  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define nl      "\n"
+#define MULTITEST 1
+
+void solution () {
+    int h, m; cin >> h >> m;
+    int ans = 60 - m;
+    ans += ((24-h-1) * 60);
+    cout << ans << nl;
+}
+
+int main (void) {
+    FastIO;
+    int tc=1; if (MULTITEST) cin>>tc;
+    for(int cs=1;cs<=tc;cs++) solution();
+    return 0;
+}
