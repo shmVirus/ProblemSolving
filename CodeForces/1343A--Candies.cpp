0e58a0d9c6a7c362e2ef5647f7cb33216e49da28
Sabbir <sabbir@disroot.org>
2023-07-18 21:37:25 +0600

    solve codeforces 1343A

---
+++ CodeForces/1343A--Candies.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastIO  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define nl      "\n"
+#define MULTITEST 1
+
+void solution () {
+    int n; cin >> n;
+    for (int pw = 2; pw < 30; ++pw) {
+        int val = (1 << pw) - 1;
+        if (n % val == 0) {
+            cout << n / val << nl;
+            break;
+        }
+    }
+}
+
+int main (void) {
+    FastIO;
+    int tc=1; if (MULTITEST) cin>>tc;
+    for(int cs=1;cs<=tc;cs++) solution();
+    return 0;
+}
