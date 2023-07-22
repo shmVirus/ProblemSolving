4bc0db9a3d5a11ddeb9200e37ed3469d759f8211
Sabbir <sabbir@disroot.org>
2023-07-22 20:39:37 +0600

    solve codeforces 1850A

---
+++ CodeForces/1850A--To_My_Critics.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastIO  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define nl      "\n"
+#define MULTITEST 1
+
+void solution () {
+    int a, b, c; cin >> a >> b >> c;
+    cout << (a+b >= 10 || b+c >= 10 || c+a >= 10 ? "YES" : "NO") << nl;
+}
+
+int main (void) {
+    FastIO;
+    int tc=1; if (MULTITEST) cin>>tc;
+    for(int cs=1;cs<=tc;cs++) solution();
+    return 0;
+}
