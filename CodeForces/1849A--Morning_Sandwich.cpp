089f8951483fe27496000f736a5cfafadad763d1
Sabbir <sabbir@disroot.org>
2023-07-31 20:19:38 +0600

    solve codeforces 1849A

---
+++ CodeForces/1849A--Morning_Sandwich.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastIO  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define nl      "\n"
+#define MULTITEST 1
+
+void solution () {
+    int b, c, h; cin >> b >> c >> h;
+
+    int s = c + h;
+    cout << ( b > s ? 2 * s + 1 : 2 * b - 1 ) << nl;
+}
+
+int main (void) {
+    FastIO;
+    int tc=1; if (MULTITEST) cin>>tc;
+    for(int cs=1;cs<=tc;cs++) solution();
+    return 0;
+}
