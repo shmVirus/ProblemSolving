257c2b73b297e21401e6ee0f3ea208620781d827
Sabbir <sabbir@disroot.org>
2023-08-02 21:24:53 +0600

    solve codeforces 1385A

---
+++ CodeForces/1385A--Three_Pairwise_Maximums.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastIO  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define nl      "\n"
+#define MULTITEST 1
+
+void solution () {
+    int a[3]; cin >> a[0] >> a[1] >> a[2];
+    sort(a, a+3);
+    if (a[1] != a[2]) {
+        cout << "NO" << nl;
+    } else {
+        cout << "YES" << nl << a[0] << " " << a[0] << " " << a[2] << nl;
+    }
+}
+
+int main (void) {
+    FastIO;
+    int tc=1; if (MULTITEST) cin>>tc;
+    for(int cs=1;cs<=tc;cs++) solution();
+    return 0;
+}
