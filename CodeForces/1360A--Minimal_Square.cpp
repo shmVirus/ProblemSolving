929aa031e217331885fe80ad5e36b8dc0ce3c5ce
Sabbir <sabbir@disroot.org>
2023-08-05 20:29:35 +0600

    solve codeforces 1360A

---
+++ CodeForces/1360A--Minimal_Square.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastIO  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define nl      "\n"
+#define MULTITEST 1
+
+void solution () {
+    int a, b; cin >> a >> b;
+    int side = min(max(a * 2, b), max(a, b * 2));
+    cout << side * side << nl;
+}
+
+int main (void) {
+    FastIO;
+    int tc=1; if (MULTITEST) cin>>tc;
+    for(int cs=1;cs<=tc;cs++) solution();
+    return 0;
+}
