72d79818066a6ee51d68d91a7f52fa3b7778220d
Sabbir <sabbir@disroot.org>
2023-08-07 20:28:42 +0600

    solve codeforces 1337B

---
+++ CodeForces/1337B--Kana_and_Dragon_Quest_game.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastIO  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define nl      "\n"
+#define MULTITEST 1
+
+void solution () {
+    int x, n, m; cin >> x >> n >> m;
+	while (x>0 && n && x/2+10<x){n--; x=x/2+10;}
+    cout << (x <= m * 10 ? "YES" : "NO") << nl;
+}
+
+int main (void) {
+    FastIO;
+    int tc=1; if (MULTITEST) cin>>tc;
+    for(int cs=1;cs<=tc;cs++) solution();
+    return 0;
+}
