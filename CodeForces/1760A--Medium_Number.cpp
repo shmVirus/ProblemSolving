883b76fbb4a6d21fb64414f32ac76f1990939d01
Sabbir <sabbir@disroot.org>
2023-08-09 19:55:24 +0600

    solve codeforces 1760A

---
+++ CodeForces/1760A--Medium_Number.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastIO  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define nl      "\n"
+#define MULTITEST 1
+
+void solution () {
+    int a[3];
+    cin >> a[0] >> a[1] >> a[2];
+    sort(a, a+3);
+    cout << a[1] << nl;
+}
+
+int main (void) {
+    FastIO;
+    int tc=1; if (MULTITEST) cin>>tc;
+    for(int cs=1;cs<=tc;cs++) solution();
+    return 0;
+}
