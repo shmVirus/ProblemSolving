b5efacfa67ec43bfdc1e422f4a1baaf684347f8a
Sabbir <sabbir@disroot.org>
2023-08-15 19:36:05 +0600

    solve codeforces 1772A

---
+++ CodeForces/1772A--A_plus_B.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastIO  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define nl      "\n"
+#define MULTITEST 1
+
+void solution () {
+    string s; cin >> s;
+    cout << s[0] - 48 + s[2] - 48 << nl;
+}
+
+int main (void) {
+    FastIO;
+    int tc=1; if (MULTITEST) cin>>tc;
+    for(int cs=1;cs<=tc;cs++) solution();
+    return 0;
+}
