7f924ea434338c34338a503f12ca6acefdd92f05
Sabbir <sabbir@disroot.org>
2023-08-19 19:54:10 +0600

    solve codeforces 1527A

---
+++ CodeForces/1527A--And_Then_There_Were_K.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastIO  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define nl      "\n"
+#define MULTITEST 1
+
+void solution () {
+    long long n; cin >> n;
+    long long cnt = 0;
+    while(n != 0){
+        ++cnt;
+        n /= 2;
+    }
+    cout << (1<<(cnt-1))-1 << nl;
+}
+
+int main (void) {
+    FastIO;
+    int tc=1; if (MULTITEST) cin>>tc;
+    for(int cs=1;cs<=tc;cs++) solution();
+    return 0;
+}
