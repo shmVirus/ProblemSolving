3cc96215f2a2fee49c7b1006fadaa1799f627fea
Sabbir <sabbir@disroot.org>
2023-08-27 19:44:48 +0600

    solve codeforces 1353C

---
+++ CodeForces/1353C--Board_Moves.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastIO  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define nl      "\n"
+#define MULTITEST 1
+
+void solution () {
+    int n; cin >> n;
+    long long ans = 0;
+    for (int i = 1; i <= n / 2; ++i) ans += i * 1ll * i;
+    cout << ans * 8 << nl;
+}
+
+int main (void) {
+    FastIO;
+    int tc=1; if (MULTITEST) cin>>tc;
+    for(int cs=1;cs<=tc;cs++) solution();
+    return 0;
+}
