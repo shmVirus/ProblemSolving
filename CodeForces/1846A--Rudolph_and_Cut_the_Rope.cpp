7c917b977943fe4acbc011ad4e2dc1eaa82f3285
Sabbir <sabbir@disroot.org>
2023-08-31 19:10:05 +0600

    solve codeforces 1846A

---
+++ CodeForces/1846A--Rudolph_and_Cut_the_Rope.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastIO  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define nl      "\n"
+#define MULTITEST 1
+
+void solution () {
+    int n; cin >> n; int ans = 0;
+    for (int i = 0; i < n; ++i) {
+        int a, b; cin >> a >> b;
+        if (a > b) ++ans;
+    }
+    cout << ans << endl;
+}
+
+int main (void) {
+    FastIO;
+    int tc=1; if (MULTITEST) cin>>tc;
+    for(int cs=1;cs<=tc;cs++) solution();
+    return 0;
+}
