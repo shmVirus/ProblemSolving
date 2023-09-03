3f8a80a59bf5e2ebab10e639f446fa6741415eab
Sabbir <sabbir@disroot.org>
2023-09-03 21:06:48 +0600

    solve codeforces 1714A

---
+++ CodeForces/1714A--Everyone_Loves_to_Sleep.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastIO  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define nl      "\n"
+#define MULTITEST 1
+
+void solution () {
+    int n; cin >> n;
+    int time, h, m; cin >> h >> m;
+    time = 60 * h + m;
+    int ans = 24 * 60;
+    for(int i = 0; i < n; ++i) {
+        cin >> h >> m;
+        int t = 60 * h + m - time;
+        if(t < 0) t += 24 * 60;
+        ans = min(ans, t);
+    }
+    cout << ans / 60 << " " << ans % 60 << nl;
+}
+
+int main (void) {
+    FastIO;
+    int tc=1; if (MULTITEST) cin>>tc;
+    for(int cs=1;cs<=tc;cs++) solution();
+    return 0;
+}
