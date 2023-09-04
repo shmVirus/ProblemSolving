10086e7632b2d6a9ea400d0715bb357bc68bdead
Sabbir <sabbir@disroot.org>
2023-09-04 19:52:00 +0600

    solve codeforces 1760B

---
+++ CodeForces/1760B--Atilla_s_Favorite_Problem.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastIO  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define nl      "\n"
+#define MULTITEST 1
+
+void solution () {
+    int n; cin >> n;
+    string s; cin >> s;
+    int ans = -1;
+    for (int i = 0 ; i < n ; ++i) {
+        ans = max(ans, s[i]-'a');
+    }
+    cout << ++ans << nl;
+}
+
+int main (void) {
+    FastIO;
+    int tc=1; if (MULTITEST) cin>>tc;
+    for(int cs=1;cs<=tc;cs++) solution();
+    return 0;
+}
