53bde36e74e5d2ba44470a5fe1ebe3a95928b9c8
Sabbir <sabbir@disroot.org>
2023-09-12 19:08:01 +0600

    solve codeforces 492A

---
+++ CodeForces/492A--Vanya_and_Cubes.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastIO  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define nl      "\n"
+#define MULTITEST 0
+
+void solution () {
+    int n; cin >> n;
+    int cnt=0, h=0;
+    while (cnt <= n) {
+        ++h;
+        cnt += (h*(h+1))/2;
+    }
+    cout << h-1 << nl;
+}
+
+int main (void) {
+    FastIO;
+    int tc=1; if (MULTITEST) cin>>tc;
+    for(int cs=1;cs<=tc;cs++) solution();
+    return 0;
+}
