b9a2d33297b57786d4489c1ed0cc51902070c2e2
Sabbir <sabbir@disroot.org>
2023-09-16 18:30:41 +0600

    solve codeforces 1766A

---
+++ CodeForces/1766A--Extremely_Round.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastIO  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define nl      "\n"
+#define MULTITEST 1
+
+void solution () {
+    string n; cin >> n;
+    cout << ((n.size()-1) * 9 ) + n[0]-'0' << nl;
+}
+
+int main (void) {
+    FastIO;
+    int tc=1; if (MULTITEST) cin>>tc;
+    for(int cs=1;cs<=tc;cs++) solution();
+    return 0;
+}
