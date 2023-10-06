6afb3aa7c227dc2a032b2e1f975cda3dabfcf270
Sabbir <sabbir@disroot.org>
2023-10-06 21:09:40 +0600

    solve codeforces 1372A

---
+++ CodeForces/1372A--Omkar_and_Completion.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastIO  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define nl      "\n"
+#define MULTITEST 1
+
+void solution () {
+    int n; cin >> n;
+    for (int i=0; i<n; ++i) {
+        cout << "1" << " \n" [i+1 == n];
+    }
+}
+
+int main (void) {
+    FastIO;
+    int tc=1; if (MULTITEST) cin>>tc;
+    for(int cs=1;cs<=tc;cs++) solution();
+    return 0;
+}
