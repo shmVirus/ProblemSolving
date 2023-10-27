ff557bef8a3d2e5b4fa445ca7fd9c133cca19e2e
Sabbir <sabbir@disroot.org>
2023-10-27 18:48:52 +0600

    solve codeforces 1047B

---
+++ CodeForces/1047B--Cover_Points.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastIO  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define nl      "\n"
+#define MULTITEST 0
+
+void solution () {
+    int n, ans = 0; cin >> n;
+    for (int i=0, x, y; i<n; ++i) {
+        cin >> x >> y;
+        ans = max(ans, x+y);
+    }
+    cout << ans << nl;
+}
+
+int main (void) {
+    FastIO;
+    int tc=1; if (MULTITEST) cin>>tc;
+    for(int cs=1;cs<=tc;cs++) solution();
+    return 0;
+}
