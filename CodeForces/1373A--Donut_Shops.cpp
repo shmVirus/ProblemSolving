f38e4e95def8d5c3e42682747cf59e9745d3e7a0
Sabbir <sabbir@disroot.org>
2023-11-20 20:32:27 +0600

    solve codeforces 1373A

---
+++ CodeForces/1373A--Donut_Shops.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastIO  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define nl      "\n"
+#define MULTITEST 1
+
+void solution () {
+    long long a, b, c;
+    cin >> a >> b >> c;
+    cout << ( a < c ? 1 : -1 ) << " " << ( c < a * b ? b : -1 ) << nl;
+}
+
+int main (void) {
+    FastIO;
+    int tc=1; if (MULTITEST) cin>>tc;
+    for(int cs=1;cs<=tc;cs++) solution();
+    return 0;
+}
