9b93b5b08de558e995f1ddf9ab9f4b17e26e0d46
Sabbir <sabbir@disroot.org>
2023-12-01 20:17:00 +0600

    solve codeforces 1702A

---
+++ CodeForces/1702A--Round_Down_the__Price.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastIO  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define nl      "\n"
+#define MULTITEST 1
+
+void solution () {
+    string s; cin >> s;
+    int k = s.length()-1;
+    cout << fixed << setprecision(0) << stoi(s) - pow(10, k) << nl;
+}
+
+int main (void) {
+    FastIO;
+    int tc=1; if (MULTITEST) cin>>tc;
+    for(int cs=1;cs<=tc;cs++) solution();
+    return 0;
+}
