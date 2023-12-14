be20cd9983db56f12301e804ff600acb54f248fa
Sabbir <sabbir@disroot.org>
2023-12-14 23:06:18 +0600

    solve codeforces 1721A

---
+++ CodeForces/1721A--Image.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastIO  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define nl      "\n"
+#define MULTITEST 1
+
+void solution () {
+    string s1, s2;
+    cin >> s1 >> s2;
+    s1 += s2;
+    cout << set<char>(s1.begin(), s1.end()).size() - 1 << nl;
+}
+
+int main (void) {
+    FastIO;
+    int tc=1; if (MULTITEST) cin>>tc;
+    for(int cs=1;cs<=tc;cs++) solution();
+    return 0;
+}
