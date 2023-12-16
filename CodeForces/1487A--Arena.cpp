175fd3beec007d016dde5ceeb3799a44e300c8e8
Sabbir <sabbir@disroot.org>
2023-12-16 23:56:07 +0600

    solve codeforces 1487A

---
+++ CodeForces/1487A--Arena.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastIO  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define nl      "\n"
+#define MULTITEST 1
+
+void solution () {
+    int n; cin >> n;
+    vector<int> a(n);
+    for (int& x : a) cin >> x;
+    cout << n - count(a.begin(), a.end(), *min_element(a.begin(), a.end())) << nl;
+}
+
+int main (void) {
+    FastIO;
+    int tc=1; if (MULTITEST) cin>>tc;
+    for(int cs=1;cs<=tc;cs++) solution();
+    return 0;
+}
