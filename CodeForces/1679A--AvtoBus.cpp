8ef21bbe4b5dfcd36184b97aa80500fdbadf5816
Sabbir <sabbir@disroot.org>
2023-12-18 21:24:55 +0600

    solve codeforces 1679A

---
+++ CodeForces/1679A--AvtoBus.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastIO  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define nl      "\n"
+#define MULTITEST 1
+
+void solution () {
+    long long n; cin >> n;
+    if ( n % 2 || n < 4 ) cout << -1 << nl;
+    else cout << n/6+(n%6!=0) << " " << n/4 << nl;
+}
+
+int main (void) {
+    FastIO;
+    int tc=1; if (MULTITEST) cin>>tc;
+    for(int cs=1;cs<=tc;cs++) solution();
+    return 0;
+}
