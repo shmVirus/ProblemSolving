1aa96c3607aa2087313460b3012b9172f6b1a52a
Sabbir <sabbir@disroot.org>
2023-12-25 21:17:40 +0600

    solve codeforces 1843C

---
+++ CodeForces/1843C--Sum_in_Binary_Tree.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastIO  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define nl      "\n"
+#define MULTITEST 1
+
+void solution () {
+    long long n, s=0; cin >> n;
+    while (n >= 1) {
+        s += n;
+        n /= 2;
+    }
+    cout << s << nl;
+}
+
+int main (void) {
+    FastIO;
+    int tc=1; if (MULTITEST) cin>>tc;
+    for(int cs=1;cs<=tc;cs++) solution();
+    return 0;
+}
