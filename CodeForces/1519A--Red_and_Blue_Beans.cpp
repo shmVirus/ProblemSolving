b4f014e05d4df29fb60ca63f6ff60fc0af1b8136
Sabbir <sabbir@disroot.org>
2023-12-28 21:46:55 +0600

    solve codeforces 1519A

---
+++ CodeForces/1519A--Red_and_Blue_Beans.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastIO  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define nl      "\n"
+#define MULTITEST 1
+
+void solution () {
+    long long r, b, d;
+    cin >> r >> b >> d;
+    cout << ( min(r, b)*(d+1) >= max(r, b) ? "YES" : "NO" ) << nl;
+}
+
+int main (void) {
+    FastIO;
+    int tc=1; if (MULTITEST) cin>>tc;
+    for(int cs=1;cs<=tc;cs++) solution();
+    return 0;
+}
