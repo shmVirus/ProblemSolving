a13d861871fbbaf41e9147ecb36d1ae05f8f4c69
Sabbir <sabbir@disroot.org>
2024-01-03 21:18:36 +0600

    solve codeforces 1335B

---
+++ CodeForces/1335B--Construct_the_String.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastIO  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define nl      "\n"
+#define MULTITEST 1
+
+void solution () {
+    int n, a, b;
+    cin >> n >> a >> b;
+    for (int i = 0; i < n; ++i) {
+        cout << char('a' + i % b);
+    }
+    cout << nl;
+}
+
+int main (void) {
+    FastIO;
+    int tc=1; if (MULTITEST) cin>>tc;
+    for(int cs=1;cs<=tc;cs++) solution();
+    return 0;
+}
+
