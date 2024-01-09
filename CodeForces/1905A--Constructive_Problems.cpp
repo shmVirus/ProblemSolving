3eb542f5e5dfd50d5e61c27b233d27fa23f0a9f6
Sabbir <sabbir@disroot.org>
2024-01-09 21:52:09 +0600

    solve codeforces 1905A

---
+++ CodeForces/1905A--Constructive_Problems.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastIO  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define nl      "\n"
+#define MULTITEST 1
+
+void solution () {
+    int n, m;
+    cin >> n >> m;
+    cout << max(n, m) << nl;
+}
+
+int main (void) {
+    FastIO;
+    int tc=1; if (MULTITEST) cin>>tc;
+    for(int cs=1;cs<=tc;cs++) solution();
+    return 0;
+}
