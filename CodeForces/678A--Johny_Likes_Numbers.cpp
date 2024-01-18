4d408ab279ba706419fd1e4b19bc9b28b7c6b429
Sabbir <sabbir@disroot.org>
2024-01-18 19:57:20 +0600

    solve codeforces 678A

---
+++ CodeForces/678A--Johny_Likes_Numbers.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastIO  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define nl      "\n"
+#define MULTITEST 0
+
+void solution () {
+    int n, k;
+    cin >> n >> k;
+    cout << ((n/k)+1)*k << nl;
+}
+
+int main (void) {
+    FastIO;
+    int tc=1; if (MULTITEST) cin>>tc;
+    for(int cs=1;cs<=tc;cs++) solution();
+    return 0;
+}
