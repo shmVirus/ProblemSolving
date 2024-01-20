c68ced10a61ccf6b365366fbe266cb917520733c
Sabbir <sabbir@disroot.org>
2024-01-20 20:26:50 +0600

    solve codeforces 946A

---
+++ CodeForces/946A--Partition.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastIO  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define nl      "\n"
+#define MULTITEST 0
+
+void solution () {
+    int n; cin >> n;
+    int ans = 0;
+    for (int i = 0, a; i < n; ++i) {
+        cin >> a; ans += abs(a);
+    }
+    cout << ans << nl;
+}
+
+int main (void) {
+    FastIO;
+    int tc=1; if (MULTITEST) cin>>tc;
+    for(int cs=1;cs<=tc;cs++) solution();
+    return 0;
+}
