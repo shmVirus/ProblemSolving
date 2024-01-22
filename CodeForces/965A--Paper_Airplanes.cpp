19569eabbb12230a77ee6d0d199bfc76d46cfad2
Sabbir <sabbir@disroot.org>
2024-01-22 22:29:38 +0600

    solve codeforces 965A

---
+++ CodeForces/965A--Paper_Airplanes.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastIO  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define nl      "\n"
+#define MULTITEST 0
+
+void solution () {
+    long long k, n, s, p;
+    cin >> k >> n >> s >> p;
+    cout << (k*((n+s-1)/s)+p-1)/p << nl;
+}
+
+int main (void) {
+    FastIO;
+    int tc=1; if (MULTITEST) cin>>tc;
+    for(int cs=1;cs<=tc;cs++) solution();
+    return 0;
+}
