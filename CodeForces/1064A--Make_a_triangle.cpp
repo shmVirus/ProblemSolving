bdfe606a2f2ea9d3ebadf0ff50f0010587f6c56e
Sabbir <sabbir@disroot.org>
2024-01-24 18:42:13 +0600

    solve codeforces 1064A

---
+++ CodeForces/1064A--Make_a_triangle.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastIO  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define nl      "\n"
+#define MULTITEST 0
+
+void solution () {
+    int a[3];
+    for (int i=0; i<3; ++i) cin >> a[i];
+    sort(a, a+3);
+    cout << max(0, a[2]-a[1]-a[0]+1) << nl;
+}
+
+int main (void) {
+    FastIO;
+    int tc=1; if (MULTITEST) cin>>tc;
+    for(int cs=1;cs<=tc;cs++) solution();
+    return 0;
+}
