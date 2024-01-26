f893920fcf7338d94aea2fe212a56465f31bea05
Sabbir <sabbir@disroot.org>
2024-01-26 18:35:06 +0600

    solve codeforces 1304A

---
+++ CodeForces/1304A--Two_Rabbits.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastIO  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define nl      "\n"
+#define MULTITEST 1
+
+void solution () {
+    int x, y, a, b; cin >> x >> y >> a >> b;
+    cout << ((y - x) % (a + b) == 0 ? (y - x) / (a + b) : -1) << nl;
+}
+
+int main (void) {
+    FastIO;
+    int tc=1; if (MULTITEST) cin>>tc;
+    for(int cs=1;cs<=tc;cs++) solution();
+    return 0;
+}
