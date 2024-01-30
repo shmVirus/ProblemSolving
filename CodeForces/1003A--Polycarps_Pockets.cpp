a6b8029c5c273c520299ed56ff8c7145bcb21b96
Sabbir <sabbir@disroot.org>
2024-01-30 18:45:20 +0600

    solve codeforces 1003A

---
+++ CodeForces/1003A--Polycarps_Pockets.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastIO  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define nl      "\n"
+#define MULTITEST 0
+
+void solution () {
+    int n; cin >> n;
+    int a[101] = {0};
+    int ans = 0;
+    for (int i=0, x; i<n; ++i) {
+        cin >> x;
+        a[x]++;
+        ans = max(ans, a[x]);
+    }
+    cout << ans << nl;
+}
+
+int main (void) {
+    FastIO;
+    int tc=1; if (MULTITEST) cin>>tc;
+    for(int cs=1;cs<=tc;cs++) solution();
+    return 0;
+}
