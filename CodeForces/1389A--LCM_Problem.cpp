11ab2e9877cb3849688e086f8b2be8770684a421
Sabbir <sabbir@disroot.org>
2024-02-06 20:29:00 +0600

    solve codeforces 1389A

---
+++ CodeForces/1389A--LCM_Problem.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastIO  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define nl      "\n"
+#define MULTITEST 1
+
+void solution () {
+    int l, r;
+    cin >> l >> r;
+    int t = l * 2;
+    if (t > r) cout << -1 << " " << -1 << nl;
+    else cout << l << " " << t << nl;
+}
+
+int main (void) {
+    FastIO;
+    int tc=1; if (MULTITEST) cin>>tc;
+    for(int cs=1;cs<=tc;cs++) solution();
+    return 0;
+}
